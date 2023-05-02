f25e7e5465f60ec94cfa394394fad7c1b10ee491
John Kutch <jkutch2@yahoo.com>
2023-05-01 23:09:37 -0400

    delete error

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -15,7 +15,7 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
 
